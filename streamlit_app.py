754eb325d87c7432c39a6fdf32a66c58616772e0
Sean <92123184+seanmcosker@users.noreply.github.com>
2024-11-11 22:45:43 +0000

    added form

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,21 @@ conn = sqlite3.connect('existingDB.db')
 
 st.table()
 
-lift = st.selectbox('Select exercise', 
+with st.form("input_form"):
+    st.write("Log today's top set")
+    lift = st.selectbox('Select exercise', 
              ["OHP", "Bench", "Squat", "Deadlift"])
+    weight = st.text_input("Weight")
+    reps = st.text_input("Reps")
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("Lift", lift, "weight", weight, "Reps", reps)
+        
+
+
+
+
+
+
+
