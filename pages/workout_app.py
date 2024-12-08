efa7ff7d60617897064580c0f44bc7cd12547d30
Sean <92123184+seanmcosker@users.noreply.github.com>
2024-12-08 16:23:56 +0000

    some new functionality

--- pages/workout_app.py
+++ pages/workout_app.py
@@ -30,6 +30,35 @@ with st.form("input_form"):
         #lifts.loc(len(lifts)) = [lift, weight, reps, today]
 
 
+st.header("5/3/1 Lifts")
+
+
+
+
+new_x = st.button("Change Exercises or Maxes")
+
+if new_x:
+    with st.form("exercise_form"):
+        st.write("Select exercises")
+        vp = st.text_input("Input shoulder exercise")
+        if vp:
+            vp_max = st.text_input("Max")
+        hinge = st.text_input("Hip hinge exercise")
+        if hinge:
+            hinge_max = st.text_input("Max")
+        hp = st.text_input("Horizontal Press")
+        if hp:
+            hp_max = st.text_input("Max")
+        leg = st.text_input("Leg Exercise")
+        if leg:
+            leg_max = st.text_input("Max")
+        submitted = st.form_submit_button("Submit selections")
+        if submitted:
+            st.write("Selections locked in")
+
+
+
+
 
 
 
